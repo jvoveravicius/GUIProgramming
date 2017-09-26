0de0086000180f80aaf499ab1776a45a93c320fe
psvetovas <psvetovas@yahoo.com>
2017-09-26 13:21:32 -0700

    Added task 1

---
+++ 1Task/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BUTTON = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.Task1 = new System.Windows.Forms.RadioButton();
+            this.Task2 = new System.Windows.Forms.RadioButton();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.Task3 = new System.Windows.Forms.RadioButton();
+            this.Task4 = new System.Windows.Forms.RadioButton();
+            this.Task5 = new System.Windows.Forms.RadioButton();
+            this.Task6 = new System.Windows.Forms.RadioButton();
+            this.Task6Label = new System.Windows.Forms.Label();
+            this.Task7 = new System.Windows.Forms.RadioButton();
+            this.ResulFirst = new System.Windows.Forms.TextBox();
+            this.Task7Two = new System.Windows.Forms.TextBox();
+            this.Sandaug = new System.Windows.Forms.Label();
+            this.ResultEqual = new System.Windows.Forms.Label();
+            this.ResultTask7 = new System.Windows.Forms.Label();
+            this.Task8 = new System.Windows.Forms.RadioButton();
+            this.Task9 = new System.Windows.Forms.RadioButton();
+            this.TxtBoxSize = new System.Windows.Forms.TextBox();
+            this.Task10 = new System.Windows.Forms.RadioButton();
+            this.Task11 = new System.Windows.Forms.RadioButton();
+            this.TxtFieldMy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BUTTON
+            // 
+            this.BUTTON.Location = new System.Drawing.Point(37, 211);
+            this.BUTTON.Name = "BUTTON";
+            this.BUTTON.Size = new System.Drawing.Size(75, 23);
+            this.BUTTON.TabIndex = 0;
+            this.BUTTON.Text = "BUTTON";
+            this.BUTTON.UseVisualStyleBackColor = true;
+            this.BUTTON.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(28, 22);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(173, 20);
+            this.txtBox.TabIndex = 1;
+            // 
+            // Task1
+            // 
+            this.Task1.AutoSize = true;
+            this.Task1.Location = new System.Drawing.Point(47, 70);
+            this.Task1.Name = "Task1";
+            this.Task1.Size = new System.Drawing.Size(58, 17);
+            this.Task1.TabIndex = 2;
+            this.Task1.TabStop = true;
+            this.Task1.Text = "Task 1";
+            this.Task1.UseVisualStyleBackColor = true;
+            // 
+            // Task2
+            // 
+            this.Task2.AutoSize = true;
+            this.Task2.Location = new System.Drawing.Point(47, 93);
+            this.Task2.Name = "Task2";
+            this.Task2.Size = new System.Drawing.Size(58, 17);
+            this.Task2.TabIndex = 3;
+            this.Task2.TabStop = true;
+            this.Task2.Text = "Task 2";
+            this.Task2.UseVisualStyleBackColor = true;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Location = new System.Drawing.Point(207, 25);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(28, 13);
+            this.MainLabel.TabIndex = 4;
+            this.MainLabel.Text = "Text";
+            // 
+            // Task3
+            // 
+            this.Task3.AutoSize = true;
+            this.Task3.Location = new System.Drawing.Point(47, 116);
+            this.Task3.Name = "Task3";
+            this.Task3.Size = new System.Drawing.Size(58, 17);
+            this.Task3.TabIndex = 5;
+            this.Task3.TabStop = true;
+            this.Task3.Text = "Task 3";
+            this.Task3.UseVisualStyleBackColor = true;
+            // 
+            // Task4
+            // 
+            this.Task4.AutoSize = true;
+            this.Task4.Location = new System.Drawing.Point(47, 139);
+            this.Task4.Name = "Task4";
+            this.Task4.Size = new System.Drawing.Size(58, 17);
+            this.Task4.TabIndex = 6;
+            this.Task4.TabStop = true;
+            this.Task4.Text = "Task 4";
+            this.Task4.UseVisualStyleBackColor = true;
+            // 
+            // Task5
+            // 
+            this.Task5.AutoSize = true;
+            this.Task5.Location = new System.Drawing.Point(47, 162);
+            this.Task5.Name = "Task5";
+            this.Task5.Size = new System.Drawing.Size(58, 17);
+            this.Task5.TabIndex = 7;
+            this.Task5.TabStop = true;
+            this.Task5.Text = "Task 5";
+            this.Task5.UseVisualStyleBackColor = true;
+            // 
+            // Task6
+            // 
+            this.Task6.AutoSize = true;
+            this.Task6.Location = new System.Drawing.Point(47, 185);
+            this.Task6.Name = "Task6";
+            this.Task6.Size = new System.Drawing.Size(58, 17);
+            this.Task6.TabIndex = 8;
+            this.Task6.TabStop = true;
+            this.Task6.Text = "Task 6";
+            this.Task6.UseVisualStyleBackColor = true;
+            // 
+            // Task6Label
+            // 
+            this.Task6Label.AutoSize = true;
+            this.Task6Label.Location = new System.Drawing.Point(175, 72);
+            this.Task6Label.Name = "Task6Label";
+            this.Task6Label.Size = new System.Drawing.Size(63, 13);
+            this.Task6Label.TabIndex = 9;
+            this.Task6Label.Text = "Task6Label";
+            // 
+            // Task7
+            // 
+            this.Task7.AutoSize = true;
+            this.Task7.Location = new System.Drawing.Point(107, 72);
+            this.Task7.Name = "Task7";
+            this.Task7.Size = new System.Drawing.Size(58, 17);
+            this.Task7.TabIndex = 10;
+            this.Task7.TabStop = true;
+            this.Task7.Text = "Task 7";
+            this.Task7.UseVisualStyleBackColor = true;
+            // 
+            // ResulFirst
+            // 
+            this.ResulFirst.Location = new System.Drawing.Point(178, 92);
+            this.ResulFirst.Name = "ResulFirst";
+            this.ResulFirst.Size = new System.Drawing.Size(34, 20);
+            this.ResulFirst.TabIndex = 11;
+            // 
+            // Task7Two
+            // 
+            this.Task7Two.Location = new System.Drawing.Point(235, 91);
+            this.Task7Two.Name = "Task7Two";
+            this.Task7Two.Size = new System.Drawing.Size(34, 20);
+            this.Task7Two.TabIndex = 12;
+            // 
+            // Sandaug
+            // 
+            this.Sandaug.AutoSize = true;
+            this.Sandaug.Location = new System.Drawing.Point(218, 96);
+            this.Sandaug.Name = "Sandaug";
+            this.Sandaug.Size = new System.Drawing.Size(11, 13);
+            this.Sandaug.TabIndex = 13;
+            this.Sandaug.Text = "*";
+            // 
+            // ResultEqual
+            // 
+            this.ResultEqual.AutoSize = true;
+            this.ResultEqual.Location = new System.Drawing.Point(275, 95);
+            this.ResultEqual.Name = "ResultEqual";
+            this.ResultEqual.Size = new System.Drawing.Size(13, 13);
+            this.ResultEqual.TabIndex = 14;
+            this.ResultEqual.Text = "=";
+            // 
+            // ResultTask7
+            // 
+            this.ResultTask7.AutoSize = true;
+            this.ResultTask7.Location = new System.Drawing.Point(294, 95);
+            this.ResultTask7.Name = "ResultTask7";
+            this.ResultTask7.Size = new System.Drawing.Size(37, 13);
+            this.ResultTask7.TabIndex = 15;
+            this.ResultTask7.Text = "Result";
+            // 
+            // Task8
+            // 
+            this.Task8.AutoSize = true;
+            this.Task8.Location = new System.Drawing.Point(107, 93);
+            this.Task8.Name = "Task8";
+            this.Task8.Size = new System.Drawing.Size(58, 17);
+            this.Task8.TabIndex = 16;
+            this.Task8.TabStop = true;
+            this.Task8.Text = "Task 8";
+            this.Task8.UseVisualStyleBackColor = true;
+            // 
+            // Task9
+            // 
+            this.Task9.AutoSize = true;
+            this.Task9.Location = new System.Drawing.Point(107, 116);
+            this.Task9.Name = "Task9";
+            this.Task9.Size = new System.Drawing.Size(58, 17);
+            this.Task9.TabIndex = 17;
+            this.Task9.TabStop = true;
+            this.Task9.Text = "Task 9";
+            this.Task9.UseVisualStyleBackColor = true;
+            // 
+            // TxtBoxSize
+            // 
+            this.TxtBoxSize.Location = new System.Drawing.Point(177, 137);
+            this.TxtBoxSize.Name = "TxtBoxSize";
+            this.TxtBoxSize.Size = new System.Drawing.Size(34, 20);
+            this.TxtBoxSize.TabIndex = 18;
+            // 
+            // Task10
+            // 
+            this.Task10.AutoSize = true;
+            this.Task10.Location = new System.Drawing.Point(108, 137);
+            this.Task10.Name = "Task10";
+            this.Task10.Size = new System.Drawing.Size(64, 17);
+            this.Task10.TabIndex = 19;
+            this.Task10.TabStop = true;
+            this.Task10.Text = "Task 10";
+            this.Task10.UseVisualStyleBackColor = true;
+            // 
+            // Task11
+            // 
+            this.Task11.AutoSize = true;
+            this.Task11.Location = new System.Drawing.Point(107, 160);
+            this.Task11.Name = "Task11";
+            this.Task11.Size = new System.Drawing.Size(64, 17);
+            this.Task11.TabIndex = 20;
+            this.Task11.TabStop = true;
+            this.Task11.Text = "Task 11";
+            this.Task11.UseVisualStyleBackColor = true;
+            // 
+            // TxtFieldMy
+            // 
+            this.TxtFieldMy.AutoSize = true;
+            this.TxtFieldMy.Location = new System.Drawing.Point(253, 29);
+            this.TxtFieldMy.Name = "TxtFieldMy";
+            this.TxtFieldMy.Size = new System.Drawing.Size(35, 13);
+            this.TxtFieldMy.TabIndex = 21;
+            this.TxtFieldMy.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 247);
+            this.Controls.Add(this.TxtFieldMy);
+            this.Controls.Add(this.Task11);
+            this.Controls.Add(this.Task10);
+            this.Controls.Add(this.TxtBoxSize);
+            this.Controls.Add(this.Task9);
+            this.Controls.Add(this.Task8);
+            this.Controls.Add(this.ResultTask7);
+            this.Controls.Add(this.ResultEqual);
+            this.Controls.Add(this.Sandaug);
+            this.Controls.Add(this.Task7Two);
+            this.Controls.Add(this.ResulFirst);
+            this.Controls.Add(this.Task7);
+            this.Controls.Add(this.Task6Label);
+            this.Controls.Add(this.Task6);
+            this.Controls.Add(this.Task5);
+            this.Controls.Add(this.Task4);
+            this.Controls.Add(this.Task3);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.Task2);
+            this.Controls.Add(this.Task1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.BUTTON);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BUTTON;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.RadioButton Task1;
+        private System.Windows.Forms.RadioButton Task2;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.RadioButton Task3;
+        private System.Windows.Forms.RadioButton Task4;
+        private System.Windows.Forms.RadioButton Task5;
+        private System.Windows.Forms.RadioButton Task6;
+        private System.Windows.Forms.Label Task6Label;
+        private System.Windows.Forms.RadioButton Task7;
+        private System.Windows.Forms.TextBox ResulFirst;
+        private System.Windows.Forms.TextBox Task7Two;
+        private System.Windows.Forms.Label Sandaug;
+        private System.Windows.Forms.Label ResultEqual;
+        private System.Windows.Forms.Label ResultTask7;
+        private System.Windows.Forms.RadioButton Task8;
+        private System.Windows.Forms.RadioButton Task9;
+        private System.Windows.Forms.TextBox TxtBoxSize;
+        private System.Windows.Forms.RadioButton Task10;
+        private System.Windows.Forms.RadioButton Task11;
+        private System.Windows.Forms.Label TxtFieldMy;
+    }
+}
+
---
+++ 1Task/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class MainForm : Form
+    {
+
+        static Random rnd = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            initForm('d');
+            MainLabel.Visible = false; Task6Label.Visible = false;
+
+            ResultTask7.Visible = false; ResultEqual.Visible = false; Task7Two.Visible = false; Sandaug.Visible = false; ResulFirst.Visible = false;
+
+            TxtBoxSize.Visible = false;
+
+        }
+
+
+        private void initForm(char Case)
+        {
+
+            // 12. sudarykite programą, kuri įkrovimo metu:
+            // a.nustatytų teksto laukelio plotį į 100;
+            // b.nustatytų teksto laukelio poziciją formos centre;
+            // c.nustatytų etiketės plotį į 100;
+            // d.nustatytų etiketės rėmo parametrus į “fixedsingle”;
+            // e.nustatytų etiketės poziciją taip, kad ji būtų atitraukta nuo dešiniojo ir apatinio
+            //formos kraštų po 10.
+
+            int TextBoxWidth = this.txtBox.Size.Width;
+            int TextBoxHeight = this.txtBox.Size.Height;
+
+            int LabelWidth = this.TxtFieldMy.Size.Width;
+            int LabelHeight = this.TxtFieldMy.Size.Height;
+
+            int width = this.Width;
+            int height = this.Height;
+
+
+            switch (Case)
+            {
+                case 'a':
+
+                    this.txtBox.Size = new System.Drawing.Size(100, TextBoxHeight);
+
+                    break;
+                case 'b':
+
+                    txtBox.Location = new Point(width/2, height/2);
+                    break;
+
+                case 'c':
+                    this.TxtFieldMy.Size = new System.Drawing.Size(100, TextBoxHeight);
+                    break;
+
+                case 'd':
+                    TxtFieldMy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    break;
+                case 'e':
+                    TxtFieldMy.Location = new Point(width - LabelWidth - 25, height - LabelHeight - 48);
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+        }
+
+
+
+        private String RandT(int Type, int RandLenght = 0)
+        {
+            String Result = "";
+            int i = 0;
+            double e = 0;
+
+            String[] Names = new String[5] { "Josh", "Edward", "Loui", "Peter", "Deborah" };
+            String[] SecondName = new String[5] { "Pivn", "Madera", "Swetout", "Dolgin", "Catar" };
+
+
+            if (Type == 0)
+            {
+                i = rnd.Next(0, Names.Length);
+                Result = Names[i] + " " + SecondName[i];
+            }
+            else if (Type == 1)
+            {
+                i = rnd.Next(0, Names.Length);
+                Result = SecondName[i];
+            }
+            else if (Type == 3)
+            {
+                i = rnd.Next(0, RandLenght);
+                Result = i.ToString();
+            }
+            else if (Type == 4)
+            {
+                e = rnd.NextDouble() * (100.0 - 0) + 0;
+                Result = Math.Round((Decimal)e, 3, MidpointRounding.AwayFromZero).ToString();
+            }
+
+            return Result;
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            this.txtBox.AutoSize = false;
+
+            if (!Task2.Checked) { MainLabel.Visible = false; };
+            if (!Task6.Checked) { Task6Label.Visible = false; };
+            if (!Task9.Checked) { TxtBoxSize.Visible = false; };
+
+
+            if (!Task7.Checked)
+            {
+                ResultTask7.Visible = false; ResultEqual.Visible = false; Task7Two.Visible = false; Sandaug.Visible = false;
+                ResulFirst.Visible = false;
+
+            }
+
+            if (Task1.Checked)
+            {
+                //1. Sudarykite programą, kuri mygtuko paspaudimu teksto laukelyje(TextBox)
+                //atspausdina Jūsų vardą
+
+                txtBox.Text = RandT(0);
+            }
+            else if (Task2.Checked)
+            {
+
+                //2. Sudarykite programą, kuri mygtuko paspaudimu etiketėje(Label) atspausdina Jūsų
+                //pavardę.
+
+                txtBox.Text = RandT(1);
+                MainLabel.Visible = true;
+                MainLabel.Text = RandT(1);
+            }
+
+            else if (Task3.Checked)
+            {
+                //3. Sudarykite programą, kuri mygtuko paspaudimu etiketėje atspausdina Jūsų amžių
+                //metais.Naudokite kintamąjį.
+
+                short myyars = 30;
+                txtBox.Text = myyars.ToString();
+
+            }
+            else if (Task4.Checked)
+            {
+                //4. Sudarykite programą, kuri mygtuko paspaudimu etiketėje atspausdina atsitiktinį
+                //sveikąjį skaičių iš intervalo[0, 99]
+
+                txtBox.Text = RandT(3, 100);
+
+            }
+            else if (Task5.Checked)
+            {
+                //5. sudarykite programą, kuri mygtuko paspaudimu etiketėje atspausdina atsitiktinį realųjį
+                //skaičių iš intervalo[0, 99].
+                txtBox.Text = RandT(4);
+
+            }
+            else if (Task6.Checked)
+            {
+
+                //6. Sudarykite programą, kuri mygtuko paspaudimu etiketėse atspausdina du atsitiktinius
+                //skaičius ir jų sumą.
+
+                String a = RandT(3, 100);
+                String b = RandT(3, 100);
+                int FinalResult = Convert.ToInt16(a) + Convert.ToInt16(b);
+
+                Task6Label.Visible = true;
+                Task6Label.Text = a + "+" + b + "=" + FinalResult.ToString();
+
+            }
+            else if (Task7.Checked)
+            {
+
+                //7Sudarykite programą, kuri mygtuko paspaudimu etiketėje atspausdina dviejų, teksto
+                //laukeliuose vartotojo nurodytų skaičių sandaugą.
+
+
+
+                Task7Two.Visible = true; Sandaug.Visible = true;
+                ResulFirst.Visible = true;
+
+
+                if (ResulFirst.Text != "" && Task7Two.Text != "")
+                {
+                    int ResultValue = Convert.ToInt32(ResulFirst.Text) + Convert.ToInt32(Task7Two.Text);
+                    ResultTask7.Text = ResultValue.ToString();
+                    ResultTask7.Visible = true; ResultEqual.Visible = true;
+                }
+
+            }
+            else if (Task8.Checked)
+            {
+
+                //8.Sudarykite programą, kuri mygtuko paspaudimu padidina teksto laukelio plotį vienetu.
+                // int lineHeightPixel = TextRenderer.MeasureText("X", txtBox.Font).Width;
+
+                int TextBoxWidth = this.txtBox.Size.Width;
+                int TextBoxHeight = this.txtBox.Size.Height;
+
+                this.txtBox.Size = new System.Drawing.Size(TextBoxWidth + 1, TextBoxHeight);
+
+            }
+            else if (Task9.Checked)
+            {
+                //9. Sudarykite programą, kuri mygtuko paspaudimo metu padidina teksto laukelio plotį
+                //kitame teksto laukelyje nurodytu dydžiu.
+
+                TxtBoxSize.Visible = true;
+
+                int TextBoxWidth = this.txtBox.Size.Width;
+                int TextBoxHeight = this.txtBox.Size.Height;
+                int WidthData = 0;
+
+                if (TxtBoxSize.Text != "")
+                {
+
+                    WidthData = Convert.ToInt32(TxtBoxSize.Text);
+                    this.txtBox.Size = new System.Drawing.Size(TextBoxWidth + WidthData, TextBoxHeight);
+
+                }
+
+            }
+            else if (Task10.Checked)
+            {
+
+                //10. Sudarykite programą, kuri mygtuko paspaudimo metu jo poziciją nustatytų pagal
+                //koordinates(10, 25)
+
+                BUTTON.Location = new Point(10, 25);
+
+
+            }
+            else if (Task11.Checked)
+            {
+
+                //11. Sudarykite programą, kuri mygtuko paspaudimo metu jį perkeltų į atsitiktinę vietą
+                //formoje
+
+
+                int ButtonWidth = this.BUTTON.Size.Width;
+                int width = this.Width;
+                int rWidth = Convert.ToInt32(RandT(3, width));
+
+                int height = this.Height;
+                int rHeight = Convert.ToInt32(RandT(3, height));
+                int ButtonHeight = this.BUTTON.Size.Height;
+
+
+                if (rWidth >= width - ButtonWidth - 15)
+                {
+                    rWidth = width - ButtonWidth - 15;
+
+                }
+
+                if (rHeight >= height - ButtonHeight - 38)
+                {
+                    rHeight = height - ButtonHeight - 38;
+
+                }
+
+                BUTTON.Location = new Point(rWidth, rHeight);
+            }
+         
+            else
+            {
+
+                txtBox.Text = "Choose radio button.";
+
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
